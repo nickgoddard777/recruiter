cfb5c1f84aafbfb8a26f413c419ceff24debfe1e
Nick Goddard <nicko777@gmail.com>
2010-08-17 22:37:08 +0100

    Done with static pages

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe PagesController do
+  integrate_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+                               "Recruiter | Home")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title",
+                               "Recruiter | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title",
+                               "Recruiter | About")
+    end
+  end
+end
